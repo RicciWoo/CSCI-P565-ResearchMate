8048f5554483c2a8a8935b45fb0f60c21a56bce0
Shubham Basu <shubasu@iu.edu>
2017-10-14 11:46:32 -0400

    Restructure the project

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -1,15 +1,25 @@
-var myApp = angular.module('myApp',[]);
 
-myApp.controller('loginController', ['$scope','$http', function($scope,$http) {
+
+myApp.controller('loginController', ['$scope','$http','$location', function($scope,$http,$location) {
   var self=$scope; 
+ 
     $scope.submit = function(){
-      console.log($scope.email);
+  
       console.log($scope.pwd);
       $http.get('https://jsonplaceholder.typicode.com/posts/1').then(function(response) {
-        
+        $location.path("/singup");
               
       });
     } 
 
  
 }]);
+
+myApp.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+      templateUrl : "components/login/login.html"
+  }).when("/signup", {
+    templateUrl : "components/signup/signup.html"
+});;
+});
