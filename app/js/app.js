1237e4d05606a550a569b0ea4cc1eca6a68c090b
Shubham Basu <shubasu@iu.edu>
2017-10-15 01:03:03 -0400

    login changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,1 @@
-var myApp = angular.module('myApp',['ngRoute','ngCookies']);
-myApp.config(function($locationProvider) {
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-    });
-});
+var myApp = angular.module('myApp',['ngRoute']);
