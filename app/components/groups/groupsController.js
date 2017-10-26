25828d8bc28053e834ee8902e1d62623499187e4
Gulshan Madhwani <gmadhwan@iu.edu>
2017-10-25 21:16:57 -0400

    publication and groups page added

---
+++ app/components/groups/groupsController.js
@@ -0,0 +1,60 @@
+myApp.controller('groupsController',['$scope','$http','URL','$location', '$cookies', '$cookieStore',function($scope,$http,URL,$location, $cookies, $cookieStore) {
+  $http({
+    url: URL+"/getAllGroups",
+    method: "POST",
+    data:{},
+  }).then(function success(response){
+    $scope.groupInfo = response.data.msg.groupInfo;
+  },
+function error(response){
+});
+$scope.userGroupID = []
+$scope.username = $cookies.get('username');
+$scope.sessionString = $cookies.get('sessionString');
+$http({
+  url: URL+"/getUserGroups",
+  method: "POST",
+  data:{'username': $scope.username},
+}).then(function success(response){debugger
+  if(response.status == 200){
+    if(response.data.status == "true" && response.data.msg.groupInfo!=undefined){
+      var grpInfo = response.data.msg.groupInfo;
+      debugger
+      for(var i = 0;i<grpInfo.length;i++){
+        $scope.userGroupID.push(grpInfo[i].groupID);
+      }
+    }
+  }
+  else{
+    console.log("Error encountered while getting user groups");
+  }
+},
+function error(response){
+
+});
+  $scope.joinGroup = function(event, groupName){
+    if(event.currentTarget.value == "Joined")
+      return; //user is already in the group.
+
+      $http({
+        url: URL+"/setUserGroup",
+        method: "POST",
+        data:{'sessionString': $scope.sessionString, 'groupname': groupName},
+      }).then(function success(response){debugger
+        if(response.status == 200){
+          if(response.data.status == "true" && response.data.msg == "group entry added."){
+            event.currentTarget.value = "Joined"
+          }
+          else{
+            alert(response.data.msg);
+          }
+        }
+        else{
+          alert("Error encountered while joining the group. Please try again!")
+        }
+      },
+      function error(response){
+      });
+  }
+
+}]);
