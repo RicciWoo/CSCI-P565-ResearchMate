365697aeb7c9ea836175c1b6719321092f3e52ef
Gulshan Madhwani <gmadhwan@iu.edu>
2017-12-01 03:53:34 -0500

    connection accept reject

--- app/js/indexController.js
+++ app/js/indexController.js
@@ -1,6 +1,5 @@
-myApp.controller('indexController', ['$scope','$location', function ($scope, $location) {
+myApp.controller('indexController', ['$scope','$location','URL','$cookies','$cookieStore', '$http', function ($scope, $location, URL, $cookies, $cookieStore, $http) {
   $scope.searchQuery = function(){
-    debugger
     if($scope.searchStr.indexOf('=')!=-1){
       alert("Possible attempt of SQL Injection. Request blocked!");
       return;
@@ -8,6 +7,92 @@ myApp.controller('indexController', ['$scope','$location', function ($scope, $lo
     }
     $location.path("/search/").search({'searchStr':$scope.searchStr})
   };
+
+  var sessionString = $cookies.get('sessionString');
+  if(sessionString != undefined && sessionString.trim()!=""){
+    $http({
+      url: URL + "/getAllConnectionRequests",
+      method: "POST",
+      data:{
+        'sessionString': sessionString
+      },
+    }).then(function success(response){
+      if(response.status == 200 && response.data.status == "true" && response.data.msg!=undefined){
+        var data = response.data.msg;
+        $scope.connectionReqCount = data.users.length;
+
+        $scope.userInfo = [];
+
+        for(var i=0; i<data.userInfo.length;i++){
+          $scope.userInfo[data.userInfo[i].userID] = data.userInfo[i];
+        }
+        $scope.users = data.users;
+      }
+      else{
+        console.log(response.data.msg);
+      }
+    },
+  function error(response){
+    console.log(response.statusText);
+  });
+  }
+
+
+  function removeUserFromList(username){
+    for(var i=0;i<$scope.users.length;i++){
+      if($scope.users[i].userName == username){
+        $scope.users.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  $scope.acceptRequest = function(username){
+    if(username == "")
+      return;
+    $http({
+      url: URL + "/allowConnectionRequest",
+      method: "POST",
+      data:{
+        'sessionString' : sessionString,
+        'username': username
+      }
+    }).then(function success(response){
+      if(response.status == 200 && response.data.status == "true"){
+          removeUserFromList(username);
+      }
+      else{
+        console.log(response.data.msg);
+      }
+    },
+  function error(response){
+    console.log(response.statusText);
+  });
+  };
+
+  $scope.rejectRequest = function(username){
+    if(username == "")
+      return;
+    $http({
+      url: URL + "/rejectConnectionRequest",
+      method: "POST",
+      data:{
+        'sessionString' : sessionString,
+        'username': username
+      }
+    }).then(function success(response){
+      if(response.status == 200 && response.data.status == "true"){
+          removeUserFromList(username);
+      }
+      else{
+        console.log(response.data.msg);
+      }
+    },
+  function error(response){
+    console.log(response.statusText);
+  });
+  };
+
 
   $scope.signOut = function() {
 
